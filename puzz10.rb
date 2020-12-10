7892249314c00cf1d6935aa2f3832c5702ccf5bb
Tom Harley <tmharley@users.noreply.github.com>
2020-12-10 09:42:57 -0500

    Day 10 - add comments

--- puzz10.rb
+++ puzz10.rb
@@ -21,16 +21,23 @@ def part_one(input)
 end
 
 def part_two(input)
-  input << 0
+  input << 0 # account for 0 at the charging port
   max = input.max
-  num_paths = Array.new(max + 1, 0)
-  num_paths[-1] = 1
-  nodes = Array.new(max, false)
+  num_paths = Array.new(max + 1, 0) # how many ways to get from here to target?
+  num_paths[-1] = 1 # from target to target, one way to get there by definition
+  nodes = Array.new(max, false) # map numbers to boolean array for faster lookup
   input.each { |item| nodes[item] = true }
-  (0..max).reverse_each do |num|
-    next unless nodes[num]
-    next if num_paths[num].positive?
 
+  # Work backwards. Figure out the final approaches first, then build upon
+  # that. If we know there are, e.g. 40000 ways to get from 100 to the target,
+  # we don't have to keep recalculating those paths over and over.
+  (0..max).reverse_each do |num|
+    next unless nodes[num] # skip numbers that aren't in the list
+    next if num_paths[num].positive? # we've already looked at this number
+
+    # We can get from here to any of the next three numbers, so we can just
+    # add the numbers of already-calculated paths from those numbers to the
+    # target to figure out the number of paths from here.
     num_paths[num] = (1..3).map do |offset|
       num + offset > max ? 0 : num_paths[num + offset]
     end.reduce(&:+)
