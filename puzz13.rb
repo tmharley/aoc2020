8817037e51c9a8fa2cea727c3abcab79a5f038c9
Tom Harley <tmharley@users.noreply.github.com>
2020-12-13 09:17:24 -0500

    Day 13 documentation

--- puzz13.rb
+++ puzz13.rb
@@ -19,21 +19,26 @@ def part_one(input)
   buses[departures.index(our_departure)] * (our_departure - timestamp)
 end
 
+# Find the interval at which the condition is true for the first two buses.
+# Once we know that, we can step forward at that interval and check for the
+# third bus. That gives us the interval at which the first three buses will
+# repeat. Repeat the process adding one more bus each time until we have them
+# all.
 def part_two(input)
-  _, bus_list = input.split("\n")
+  _, bus_list = input.split("\n") # discard first line
   buses = bus_list.split(',').map { |bus| bus == 'x' ? nil : bus.to_i }
   step = buses.first
   time = prev_time = 0
 
   buses.each_with_index do |bus, offset|
-    next if bus.nil?
-    next if offset.zero?
+    next if bus.nil? # 'x' in original input
+    next if offset.zero? # skip first bus
 
     index = 0
     found_once = false
     loop do
       if ((time + offset) % bus).zero?
-        if found_once || bus == buses.last
+        if found_once || bus == buses.last # don't need interval for last bus
           step = time - prev_time
           break
         else
